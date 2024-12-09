499b6affc94a0256bbc8b0b2d87799e66b565d7e
Thraix <thraix@hotmail.com>
2024-12-09 22:55:27 +0100

    Optimize Day6 a bit

--- AdventOfCode2024/src/days/Day06.cpp
+++ AdventOfCode2024/src/days/Day06.cpp
@@ -14,33 +14,11 @@ namespace day06
     return Input::ReadArray2D(input);
   }
 
-  std::set<Index2D> GetVisited(const Array2D<char>& map)
-  {
-    std::set<Index2D> visited;
-    Index2D index = map.Find('^');
-    Index2D dir{0, -1};
-    while (map.IsInside(index))
-    {
-      visited.emplace(index);
-      Index2D newIndex{index.x + dir.x, index.y + dir.y};
-      if (map.IsInside(newIndex) && map.Get(newIndex) == '#')
-      {
-        Index2D newDir{-dir.y, dir.x};
-        dir = newDir;
-      }
-      else
-      {
-        index = newIndex;
-      }
-    }
-    return visited;
-  }
-
-  bool TestObstacle(const Array2D<char>& map, Index2D obstacle)
+  bool TestObstacle(const Array2D<char>& map, Index2D obstacle, Index2D startPos, Index2D startDir)
   {
     std::set<std::pair<Index2D, Index2D>> visited;
-    Index2D index = map.Find('^');
-    Index2D dir{0, -1};
+    Index2D index = startPos;
+    Index2D dir = startDir;
 
     while (map.IsInside(index) && visited.find(std::pair{index, dir}) == visited.end())
     {
@@ -61,18 +39,51 @@ namespace day06
 
   OUTPUT1(input)
   {
-    return GetVisited(input).size();
+    std::set<Index2D> visited;
+    Index2D index = input.Find('^');
+    Index2D dir{0, -1};
+    while (input.IsInside(index))
+    {
+      visited.emplace(index);
+      Index2D newIndex{index.x + dir.x, index.y + dir.y};
+      if (input.IsInside(newIndex) && input.Get(newIndex) == '#')
+      {
+        Index2D newDir{-dir.y, dir.x};
+        dir = newDir;
+      }
+      else
+      {
+        index = newIndex;
+      }
+    }
+    return visited.size();
   }
 
   OUTPUT2(input)
   {
-    std::set<Index2D> visited = GetVisited(input);
-
-    Index2D start = input.Find('^');
+    std::set<Index2D> visited;
+    Index2D index = input.Find('^');
+    Index2D dir{0, -1};
     int32_t sum = 0;
-    for (auto visit : visited)
-      if (visit != start && TestObstacle(input, visit))
-        sum++;
+    while (input.IsInside(index))
+    {
+      Index2D newIndex{index.x + dir.x, index.y + dir.y};
+      if (input.IsInside(newIndex) && input.Get(newIndex) == '#')
+      {
+        Index2D newDir{-dir.y, dir.x};
+        dir = newDir;
+      }
+      else
+      {
+        if (visited.count(newIndex) == 0)
+        {
+          if (TestObstacle(input, newIndex, index, dir))
+            sum++;
+        }
+        visited.emplace(index);
+        index = newIndex;
+      }
+    }
 
     return sum;
   }
